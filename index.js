f1f889af3913860601d0a0f59209327506adca8a
Padmapriyakarthik <71600824+Padmapriyakarthik@users.noreply.github.com>
2021-02-17 13:29:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -353,7 +353,7 @@ async function getPlaylistWithImage(givenurl)
          for(i=0;i<data.items.length;i++)
         {
             var col_img=document.createElement('div');
-            col_img.setAttribute('class','col-sm-12  col-md-6 col-lg-2  m-3');
+            col_img.setAttribute('class','col-sm-12  col-md-6 col-lg-2 m-3');
             var img=document.createElement('img');
             if(data.items[i].images!=null)
             {
